b8e88e5b58b1ffd92d9ac9d3afec310839235118
Sergiu Miron <Sergiu.Miron@endava.com>
2019-11-20 16:15:07 +0200

    Added a table to show all projects in order to test the get method

--- InternshipManagement/WebApp/src/app/entities/projects/projects.component.ts
+++ InternshipManagement/WebApp/src/app/entities/projects/projects.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {ProjectModel} from './project.model';
-import {ProjectService} from "./project.service";
+import {ProjectService} from './project.service';
 
 @Component({
   selector: 'app-projects',
@@ -10,16 +10,26 @@ import {ProjectService} from "./project.service";
 export class ProjectsComponent implements OnInit {
 
   project: ProjectModel = new ProjectModel();
+  arrayOfProjects: ProjectModel[] = [];
 
   constructor(private projectService: ProjectService) { }
 
   ngOnInit() {
+    this.initProjects();
+  }
+
+  initProjects() {
+    this.projectService.getAll().subscribe(res => {
+      console.log('all projects: ', res);
+      this.arrayOfProjects = res;
+    });
   }
 
   save() {
     console.log('saved', this.project);
     this.projectService.create(this.project).subscribe(res => {
       console.log(res);
+      this.initProjects();
     });
   }
 
